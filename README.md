5f57e2e44f0888f20bf8da277fae19859b230fd7
Yiding He <yiding.he@gmail.com>
2024-10-28 15:27:27 +0800

    WIP: Extracting strings to src/main/resources/i18n

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ GUI wrapper for [wkhtmltopdf](https://wkhtmltopdf.org)
 This project refers to my another open source javafx library [hydrogen-fx](https://github.com/yiding-he/hydrogen-fx),
 so if you want to build this please clone hydrogen-fx first and run `mvn install`.
 
+
+### Note for packaging:
+
+Package with `mvn package` will get 2 packaged files: `hydrogen-wkhtmltopdf.jar` 
+and `hydrogen-wkhtmltopdf-jar-with-dependencies.jar`.
+The latter one is a fat jar that contains all dependencies, 
+so it can be run using `java -jar hydrogen-wkhtmltopdf-jar-with-dependencies`.
+
+### Switch Language:
+
+When running with `java -jar` command, you can switch language by adding 
+`-Duser.language=en` or `-Duser.language=zh_CN` to the command. Currently, 
+only English and Chinese are supported.
+
 ### Screenshots:
 
 ![](images/0.png)
--- src/main/java/com/hyd/wkhtmltopdf/WkhtmltopdfApplication.java
+++ src/main/java/com/hyd/wkhtmltopdf/WkhtmltopdfApplication.java
@@ -3,11 +3,14 @@ package com.hyd.wkhtmltopdf;
 import com.hyd.fx.Fxml;
 import com.hyd.fx.app.AppLogo;
 import com.hyd.fx.app.AppPrimaryStage;
+import com.hyd.fx.i18n.XMLResourceBundleControl;
 import javafx.application.Application;
 import javafx.application.HostServices;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
 
 public class WkhtmltopdfApplication extends Application {
@@ -23,8 +26,11 @@ public class WkhtmltopdfApplication extends Application {
 
         hostServices = getHostServices();
 
+        var resourceBundle = ResourceBundle.getBundle("i18n.strings", new XMLResourceBundleControl());
+        var fxmlLoader = Fxml.load("/main.fxml", resourceBundle);
+
         primaryStage.setTitle("WKHTMLTOPDF");
-        primaryStage.setScene(new Scene(Fxml.load("/main.fxml").getRoot(), 850, 650));
+        primaryStage.setScene(new Scene(fxmlLoader.getRoot(), 850, 650));
         primaryStage.show();
     }
 }
